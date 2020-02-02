e0e1cd4c392f500cd3b82220183768c8cc1adf48
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2020-02-02 21:39:33 +0400

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <template>
   <div>
     <h1>Parent: {{carName}}</h1>
-    <app-car 
+    <app-counter :counter="counter"></app-counter>
+    <app-car
     :carName="carName"
     :carYear="carYear"
+    :counter="counter"
+    :changeFunc = "changeNameToAudi"
+    @nameChanged = " carName = $event"
+    @counterUpdated = "counter = $event"
     > </app-car>
   </div>
 </template>
 
 <script>
 import Car from './Car.vue'
+import Counter from "./Counter";
 export default {
   name: 'app',
   data () {
     return {
-      carName: 'Ford from parent',
-      carYear: 2020
+      carName: 'Ford',
+      carYear: 2020,
+      counter: 0
+    }
+  },
+  methods: {
+    changeNameToAudi() {
+      this.carName = 'Audi'
     }
   },
   components: {
-    appCar: Car
+    appCar: Car,
+    appCounter: Counter
   }
 }
 </script>
---
+++ src/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>{{counter}}</h1>
+</template>
+
+<script>
+    export default {
+        props: ['counter']
+    }
+</script>
+
+<style scoped>
+
+</style>
