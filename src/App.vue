9618dd34fae88c42c149a06ec97bcd76d0363064
Alucard163 <romemix163@gmail.com>
2020-02-02 19:33:55 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Parent: {{carName}}</h1>
+    <app-car 
+    :carName="carName"
+    :carYear="carYear"
+    > </app-car>
+  </div>
+</template>
+
+<script>
+import Car from './Car.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      carName: 'Ford from parent',
+      carYear: 2020
+    }
+  },
+  components: {
+    appCar: Car
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
